2d7f5e0e59415c42a1092638b89f35dad86e4d95
bjohns2 <bjohns@mit.edu>
2016-10-23 19:52:32 -0400

    wow

--- src/commands/move.js
+++ src/commands/move.js
@@ -17,18 +17,12 @@ const handler = (payload, res) => {
 
     var attachments = repos.slice(0, 5).map((repo) => {
       return {
-        title: `Wow a move!`,
-        // title_link: repo.url,
-        text:  ```| X | O | O |
-                  |---+---+---|
-                  | O | X | X |
-                  |---+---+---|
-                  | X | O | X |```,
+        title: `${repo.owner}/${repo.title} `,
+        title_link: repo.url,
+        text: `_${repo.description}_\n${repo.language} • ${repo.star}>`,
         mrkdwn_in: ['text', 'pretext']
       }
     })
-
- 
 
     let msg = _.defaults({
       channel: payload.channel_name,
@@ -41,4 +35,4 @@ const handler = (payload, res) => {
   })
 }
 
-module.exports = { pattern: /repos/ig, handler: handler }
+module.exports = { pattern: /move/ig, handler: handler }
